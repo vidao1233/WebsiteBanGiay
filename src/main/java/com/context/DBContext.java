e1b52ccebb93e3f5e9f8c0df9ee6ab487c352df2
Yoo2309 <116976024+Yoo2309@users.noreply.github.com>
2022-12-19 23:48:11 +0700

    cart total money

--- src/main/java/com/context/DBContext.java
+++ src/main/java/com/context/DBContext.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBContext {
-	private final String serverName = "DESKTOP-FR5B65D";
+	private final String serverName = "KHANH-PC";
 	private final String dbName = "QuanLyBanGiay";
 	private final String portNumber = "1433";
-	private final String instance = "";// MSSQLSERVER LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+	private final String instance = "SQLEXPRESS";// MSSQLSERVER LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
 	private final String userID = "sa";
-	private final String password = "vidao1233";
+	private final String password = "123456";
 	public Connection getConnection() throws Exception {
 		String url = "jdbc:sqlserver://" + serverName + "\\" + instance + ";databaseName=" + dbName;
 		if (instance == null || instance.trim().isEmpty())
